57947eebeb09af4e97e5b7da4e986686bb795e24
X Nagireddy <xnagireddy@gmail.com>
2018-07-13 18:52:00 +0530

    Added hunter_box_1 sample to the inotify-tools branch.

--- cmake/projects/hunter_box_1/hunter.cmake
+++ cmake/projects/hunter_box_1/hunter.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2018-2018, X Nagireddy
+# Copyright (c) 2016-2018, Ruslan Baratov
 # All rights reserved.
 
 # !!! DO NOT PLACE HEADER GUARDS HERE !!!
@@ -10,15 +10,15 @@ include(hunter_pick_scheme)
 
 hunter_add_version(
     PACKAGE_NAME
-    inotify-tools
+    hunter_box_1
     VERSION
-    3.20.1
+    1.0.0
     URL
-    "https://github.com/xnagireddy/inotify-tools/archive/hunterized.tar.gz"
+    "https://github.com/hunterbox/hunter_box_1/archive/v1.0.0.tar.gz"
     SHA1
-    9f9d2d3af3020fbb8902c46e99ea28e92b580106
+    4fa7fe75629f148a61cedc6ba0bce74f177a6747
 )
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
-hunter_cacheable(inotify-tools)
-hunter_download(PACKAGE_NAME inotify-tools)
+hunter_cacheable(hunter_box_1)
+hunter_download(PACKAGE_NAME hunter_box_1)
