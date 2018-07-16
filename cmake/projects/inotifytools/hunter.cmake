52759f3e37927f50d9559b66203961d9f528fd6e
X Nagireddy <xnagireddy@gmail.com>
2018-07-16 10:48:45 +0530

    Renamed inotify-tools to inotifytools.

--- cmake/projects/inotifytools/hunter.cmake
+++ cmake/projects/inotifytools/hunter.cmake
@@ -10,15 +10,15 @@ include(hunter_pick_scheme)
 
 hunter_add_version(
     PACKAGE_NAME
-    inotify-tools
+    inotifytools
     VERSION
     3.20.1
     URL
     "https://github.com/xnagireddy/inotify-tools/archive/hunterized.tar.gz"
     SHA1
-    9f9d2d3af3020fbb8902c46e99ea28e92b580106
+    0c9e38576bf29d1a880fb048ba2842c3fd71bce9
 )
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
-hunter_cacheable(inotify-tools)
-hunter_download(PACKAGE_NAME inotify-tools)
+hunter_cacheable(inotifytools)
+hunter_download(PACKAGE_NAME inotifytools)
