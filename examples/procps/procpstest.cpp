71df46e8b9b67443a7574c1600a20dddb2b97ded
X Nagireddy <xnagireddy@gmail.com>
2018-09-10 14:45:43 +0530

    Added procps linux version to procps test.

--- examples/procps/procpstest.cpp
+++ examples/procps/procpstest.cpp
@@ -1,4 +1,7 @@
+#include <iostream>
 #include <proc/procps.h>
 
 int main() {
+    int l_result = procps_linux_version();
+    std::out << "procps_linux_version(): [" << l_result << "]" << std::endl;
 }
